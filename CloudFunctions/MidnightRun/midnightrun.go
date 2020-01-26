7cacbe3a0aa9ad1a9d60269c524a5be3a6f06a54
Robert J <chew-z@users.noreply.github.com>
2020-01-26 09:28:25 +0100

    Simplified MidnightRun Cloud Function

--- CloudFunctions/MidnightRun/midnightrun.go
+++ CloudFunctions/MidnightRun/midnightrun.go
@@ -10,8 +10,6 @@ import (
 	"time"
 
 	"cloud.google.com/go/firestore"
-	firebase "firebase.google.com/go"
-	"github.com/zmb3/spotify"
 	"golang.org/x/oauth2"
 	"google.golang.org/api/iterator"
 )
@@ -34,36 +32,26 @@ var (
 	projectID       = os.Getenv("GOOGLE_CLOUD_PROJECT")
 	firestoreClient *firestore.Client
 	ctx             = context.Background()
-	location, _     = time.LoadLocation("Europe/Warsaw")
-	redirectURI     = os.Getenv("REDIRECT_URI")
-	auth            = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate, spotify.ScopeUserTopRead, spotify.ScopeUserLibraryRead, spotify.ScopeUserFollowRead, spotify.ScopeUserReadRecentlyPlayed, spotify.ScopePlaylistModifyPublic, spotify.ScopePlaylistModifyPrivate)
 )
 
-func init() {
-	// Use the application default credentials.
-	conf := &firebase.Config{ProjectID: projectID}
-	// Use context.Background() because the app/client should persist across invocations.
-	ctx := context.Background()
-	app, err := firebase.NewApp(ctx, conf)
-	if err != nil {
-		log.Fatalf("firebase.NewApp: %v", err)
-	}
+func main() {
+	defer firestoreClient.Close()
+}
 
-	firestoreClient, err = app.Firestore(ctx)
-	if err != nil {
-		log.Fatalf("app.Firestore: %v", err)
-	}
+func init() {
+	ctx := context.Background()
+	firestoreClient = initFirestoreDatabase(ctx)
 }
 
 /*MidnightRun - does litmus configuration
-also taks care of database maintenance
+also takes care of database maintenance
 It removes all tracks listened to more then a week ago
 TODO - it should also clean unused and failed tokens
 and users who stoped using service
 As the name suggest it is supposed to run once a day at
 midnight. But no hassle to run it by hand.
 
-PS. The movie is great https://youtu.be/LF8cT6ivlr4
+PS. The movie Midnight Run is great https://youtu.be/LF8cT6ivlr4
 */
 func MidnightRun(w http.ResponseWriter, r *http.Request) {
 	var newTok firestoreToken
@@ -76,26 +64,11 @@ func MidnightRun(w http.ResponseWriter, r *http.Request) {
 		log.Printf("An error retrieving users: %s", err.Error())
 	}
 	for _, doc := range docs {
-		// var spotifyClient spotify.Client
-		// fmt.Println(i, doc.Ref.ID)
 		user := doc.Data()["userID"].(string) // legit would to read data and get userID
 		log.Printf("user: %s", user)
 		newTok.user = user
 		newTok.path = "/user"
-		// tok, err := getTokenFromDB(&newTok)
-		if err != nil {
-			log.Println(err.Error())
-		}
-		// // log.Printf("%v", tok)
-		// spotifyClient = auth.NewClient(tok)
-		// spotifyUser, err := client.CurrentUser()
-		// fmt.Println(spotifyUser.DisplayName)
-		// -----------------------------
-		batchSize := 20
-		// user, err := spotifyClient.CurrentUser()
-		if err != nil {
-			log.Panic(err)
-		}
+		batchSize := 24
 		path := fmt.Sprintf("users/%s/recently_played", user)
 		ref := firestoreClient.Collection(path).Where("played_at", "<", time.Now().AddDate(0, 0, -7)) // 7 days
 		for {
@@ -130,12 +103,6 @@ func MidnightRun(w http.ResponseWriter, r *http.Request) {
 		}
 		//--------------------------------
 		userCounter++
-		// _, errBatch := batch.Commit(ctx)
-		// if errBatch != nil {
-		// 	// Handle any errors in an appropriate way, such as returning them.
-		// 	log.Printf("An error while commiting batch to firestore: %s", err.Error())
-		// 	log.Panic(err)
-		// }
 		log.Printf("Processed %d tracks for %d users", trackCounter, userCounter)
 	}
 	w.WriteHeader(http.StatusOK)
@@ -148,14 +115,14 @@ func MidnightRun(w http.ResponseWriter, r *http.Request) {
 /*initFirestoreDatabase - as the name says creates Firestore client
 in Google Cloud it is using project ID, on localhost credentials file
 */
-// func initFirestoreDatabase(ctx context.Context) *firestore.Client {
-// 	sa := option.WithCredentialsFile(".firebase-credentials.json")
-// 	firestoreClient, err := firestore.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"), sa)
-// 	if err != nil {
-// 		log.Panic(err)
-// 	}
-// 	return firestoreClient
-// }
+func initFirestoreDatabase(ctx context.Context) *firestore.Client {
+	// sa := option.WithCredentialsFile(".firebase-credentials.json")
+	firestoreClient, err := firestore.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
+	if err != nil {
+		log.Panic(err)
+	}
+	return firestoreClient
+}
 
 func getTokenFromDB(token *firestoreToken) (*oauth2.Token, error) {
 	path := fmt.Sprintf("users/%s/tokens%s", token.user, token.path)
@@ -167,6 +134,5 @@ func getTokenFromDB(token *firestoreToken) (*oauth2.Token, error) {
 	tok := &oauth2.Token{}
 	dsnap.DataTo(tok)
 	token.token = tok
-	// log.Printf("getTokenFromDB: Got token with expiration %s", tok.Expiry.In(location).Format("15:04:05"))
 	return tok, nil
 }
